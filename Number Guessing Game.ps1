ed8721670a3360692c041c6063e6030057006aa6
Don Gwynn <donaldgwynn85@outlook.com>
2023-05-23 17:23:24 -0700

    changed in vs code

--- Number Guessing Game.ps1
+++ Number Guessing Game.ps1
@@ -2,7 +2,7 @@
 $NumberToGuess =1..100 |Get-Random
 do {
   [int]$userguess =Read-Host -Prompt "Please enter a number between 1 and 100"
-  if ($userguess -lt $numbertoguess) {write-host "pick a higher number"  }
+  if ($userguess -lt $numbertoguess) {write-host "pick a higher number" }
   elseif ($userguess -gt $numbertoguess) {write-host "pick a lower number"  }
   else {write-host "you legend"}
 } until ($numbertoguess -eq $userguess)
