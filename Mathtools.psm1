d46a0ec5373c6d246e467e4061b695bccc146a9b
Donaldgwynn <134262252+Donaldgwynn@users.noreply.github.com>
2023-07-24 21:39:32 -0700

    Add files via upload

---
+++ Mathtools.psm1
@@ -0,0 +1,19 @@
+﻿Function Get-Addition {
+  Param (
+    $Num1,
+    $Num2
+  )
+  return $Num1 + $Num2
+}
+
+
+
+
+Function Get-Multiplication {
+  Param (
+    $Num1,
+    $Num2
+  )
+  return $Num1 * $Num2
+}
+
---
+++ Testmyfunctions.ps1
@@ -0,0 +1,12 @@
+﻿function AreaOfRectangle {
+    Param (
+      $Length,
+      $Width
+    )
+
+    $Area = $Length * $Width
+    Return $Area
+}
+
+$AreaOfObject = AreaOfRectangle -Length 5 -Width 10
+$AreaOfObject
