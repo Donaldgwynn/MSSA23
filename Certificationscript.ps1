206d9093a93571afd7dbd7615232df03bdafa0eb
Donaldgwynn <134262252+Donaldgwynn@users.noreply.github.com>
2023-07-24 18:15:39 -0700

    Add files via upload

---
+++ Certificationscript.ps1
@@ -0,0 +1,4 @@
+﻿$cert = get-childitem -path cert:\currentuser\my -CodeSigningCert
+Set-AuthenticodeSignature -Filepath E:\Cert.ps1 -Certificate $Cert
+
+Set-ExecutionPolicy -Executionpolicy allsigned -scope CurrentUser
