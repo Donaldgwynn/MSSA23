20506de007870865d1a458cb566ee967e8ee12e1
Donaldgwynn <134262252+Donaldgwynn@users.noreply.github.com>
2023-07-26 23:23:41 -0700

    Create Scheduled Job
    New Script

---
+++ ScheduledJob.ps1
@@ -0,0 +1,4 @@
+﻿$sjopt = New-ScheduledJobOption -RunElevated -HideInTaskScheduler 
+$sjtrg = New-JobTrigger -Once -At (Get-Date).AddMinutes(1)
+Register-ScheduledJob -Name DonSJ -ScriptBlock {'Hello'} -Trigger $sjtrg -ScheduledJobOption $sjopt
+Get-Job
